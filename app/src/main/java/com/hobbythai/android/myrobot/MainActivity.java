3f9e47f91807df69b5a1fa8515d3b38b419c5516
kally <kally.thailand@gmail.com>
2016-10-08 11:19:17 +0700

    start

---
+++ app/src/main/java/com/hobbythai/android/myrobot/MainActivity.java
@@ -0,0 +1,19 @@
+package com.hobbythai.android.myrobot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }//on create
+
+
+
+}//main
