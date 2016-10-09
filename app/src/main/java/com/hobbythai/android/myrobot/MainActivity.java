d2da1959ebc570a42c885186b986ae90d5c93b99
kally <kally.thailand@gmail.com>
2016-10-09 11:20:50 +0700

    add 4 servo

--- app/src/main/java/com/hobbythai/android/myrobot/MainActivity.java
+++ app/src/main/java/com/hobbythai/android/myrobot/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -18,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private SeekBar seekBar;
     private int anInt = 0;
+    private RadioGroup radioGroup;
+    private static final String[] strings = new String[]{"servo1=","servo2=","servo3=","servo4="};
+    private int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,32 @@ public class MainActivity extends AppCompatActivity {
         //bind widget
         textView = (TextView) findViewById(R.id.textView);
         seekBar = (SeekBar) findViewById(R.id.seekBar);
+        radioGroup = (RadioGroup) findViewById(R.id.ragServo);
+
+        //radio group controller
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                switch (checkedId) {
+
+                    case R.id.radioButton1:
+                        index = 0;
+                        break;
+                    case R.id.radioButton2:
+                        index = 1;
+                        break;
+                    case R.id.radioButton3:
+                        index = 2;
+                        break;
+                    case R.id.radioButton4:
+                        index = 3;
+                        break;
+
+                }
+
+            }//on checked
+        });
 
         //seekbar control
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -67,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
 
         //explicit
         private Context context;
-        private static final String urlSTRING = "https://dweet.io/dweet/for/ks_bot?servo1=";
+        private static final String urlSTRING = "https://dweet.io/dweet/for/ks_bot?";
 
         //constructor
         public UploadValue(Context context) {
@@ -79,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
 
             try {
 
-                String urlDweet = urlSTRING + Integer.toString(integers[0]);
+                String urlDweet = urlSTRING + strings[index] + Integer.toString(integers[0]);
                 OkHttpClient okHttpClient = new OkHttpClient();
                 Request.Builder builder = new Request.Builder();
                 Request request = builder.url(urlDweet).build();
